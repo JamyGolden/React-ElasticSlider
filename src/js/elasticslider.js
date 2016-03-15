d4e681cf7d37acd847c0a83e46cb53b627dd552b
Jamy Golden <jamygolden@gmail.com>
2016-03-15 15:54:20 +0200

    Plugin working with react component correctly - still a mess though.

--- src/js/elasticslider.js
+++ src/js/elasticslider.js
@@ -1,47 +1,129 @@
 'use strict';
 import React from 'react';
 import _ from 'lodash';
-import ElasticSliderCore from '../bower_components/elasticslider-core/dist/elasticslider-core.min';
-import ElasticSliderArrows from './elasticslider-arrows';
+import ReactDOM from 'react-dom';
+import ElasticSliderCore from '../bower_components/elasticslider-core/src/elasticslider';
+// import ElasticSliderArrows from './elasticslider-arrows';
 
 var ElasticSlider = React.createClass({
+    _disablePagi: false,
+    componentDidMount: function() {
+        this.el = this.refs.container.parentNode;
+        this.elasticSlider = new ElasticSliderCore(
+            this.el,
+            this.props.options
+        );
+
+        this._activeSlide = this.props.options.activeSlide || 1;
+        this._totalSlides = this.refs.container.children.length;
+        console.log(this._totalSlides)
+
+    },
     getInitialState: function() {
-        console.log(this.props.options)
         return {
             text: 'jamy'
         };
     },
 
+    toSlide: function(direction) {
+
+        console.log('trigger', direction, this._activeSlide)
+        var index = null;
+
+        direction = direction.toLowerCase();
+        direction = direction === 'prev' ? direction : 'next';
+
+        if (direction === 'prev'){
+            index = this.elasticSlider.getProp('activeSlideIndex') -1;
+
+            this.elasticSlider.setProp('animationDirection', 'prev');
+        } else {
+            index = this.elasticSlider.getProp('activeSlideIndex') +1;
+
+            this.elasticSlider.setProp('animationDirection', 'next');
+        }
+console.log(this.refs, index, this._totalSlides)
+        this.setActive(index);
+    },
+
+    setActive: function(index) {
+        // Throttle multiple triggers
+        if (this._disablePagi === false) {
+            var self = this;
+console.log(index)
+            if (index > this._totalSlides - 1) {
+                index = 0;
+            } else if (index < 0) {
+                index = this._totalSlides - 1;
+            }
+            console.log(index)
+
+            // for (var i = 0; i < scope.pagiArr.length; i++) {
+            //     // Set active item, remove active
+            //     scope.pagiArr[i].isActive = false;
+            //
+            //     if (scope.pagiArr[i].index === index) {
+            //         scope.pagiArr[i].isActive = true;
+            //     }
+            // }
+console.log(this.elasticSlider.toSlide)
+            this.elasticSlider.toSlide({
+                index: index,
+                animation: this.props.options.animation,
+                startAnimationCallback: function(){
+                    console.log('end')
+                    self._activeSlide = index + 1;
+                    self._disablePagi = true;
+                },
+                endAnimationCallback: function(){
+                    self._disablePagi = false;
+                }
+            });
+
+        }
+    },
+
     render: function() {
+        var arrowEl =
+            <div className="ElasticSlider-arrowList">
+                <div onClick={this.toSlide.bind(this, 'prev')}
+                    className="ElasticSlider-arrowItem ElasticSlider-arrowItem--prev">
+                    &lt;
+                </div>
+
+                <div onClick={this.toSlide.bind(this, 'next')}
+                    className="ElasticSlider-arrowItem ElasticSlider-arrowItem--next">
+                    &gt;
+                </div>
+            </div>;
+
+        var pagiEl =
+            <div className="ElasticSlider-pagiList">
+                <div ng-repeat="item in pagiArr"
+                 className="ElasticSlider-pagiItem"
+                 ng-className="{'ElasticSlider-pagiItem--isActive': item.isActive}"
+                 ng-click="setActive(item.index);">
+                </div>
+            </div>;
+
         return (
-            <div>
-            {this.props.name}
-                <div class="ElasticSlider-container">
+            <div elasticSlider="elasticSlider">
+                <div className="ElasticSlider-container"
+                 ref="container">
                     {this.props.children}
                 </div>
 
-                <div class="ElasticSlider-arrowList"
-                 ng-if="enableArrows === true">
-                    <div ng-click="toSlide('prev');"
-                     class="ElasticSlider-arrowItem ElasticSlider-arrowItem--prev">
-                        &lt;
-                    </div>
+                {(() =>
+                    this.props.options.enableArrows === true
+                        ? arrowEl
+                        : null
+                )()}
 
-                    <div ng-click="toSlide('next');"
-                     class="ElasticSlider-arrowItem ElasticSlider-arrowItem--next">
-                        &gt;
-                    </div>
-                </div>
-
-                <div ng-if="enablePagination === true">
-                    <div class="ElasticSlider-pagiList">
-                        <div ng-repeat="item in pagiArr"
-                         class="ElasticSlider-pagiItem"
-                         ng-class="{'ElasticSlider-pagiItem--isActive': item.isActive}"
-                         ng-click="setActive(item.index);">
-                        </div>
-                    </div>
-                </div>
+                {(() =>
+                    this.props.options.enablePagination === true
+                        ? pagiEl
+                        : null
+                )()}
             </div>
         );
     }
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,92 @@
-var path = require('path');
-var webpack = require('webpack');
-var pkg = require('./package.json');
-var banner = [
+const path = require('path');
+const webpack = require('webpack');
+const pkg = require('./package.json');
+const banner = [
     pkg.name + ' - ' + pkg.description,
     '@version v' + pkg.version,
     '@link ' + pkg.homepage,
     '@license ' + pkg.license,
     ''
 ].join('\n');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const PATHS = {
+    absolute: __dirname,
+    app: path.resolve(__dirname, 'src'),
+    build: path.resolve(__dirname, 'build')
+};
 
 module.exports = {
-    entry: './src/main.js',
+    entry: [
+        path.join(PATHS.app, 'main.js'),
+        'webpack-dev-server/client?http://0.0.0.0:8080', // WebpackDevServer host and port
+    ],
     output: {
-        path: __dirname,
-        filename: './dist/react-elasticslider.min.js'
+        path: PATHS.build,
+        filename: 'bundle.js'
     },
     module: {
         loaders: [
             {
-                test: /\.scss$/,
-                loaders: ['style', 'css', 'sass']
+                test: /src\/scss\/.+\.scss$/,
+                loaders: ExtractTextPlugin.extract('css!sass'),
+                include: path.resolve(PATHS.app, 'scss'),
             },
             {
-              test: /src\/.+.js$/,
-              exclude: /node_modules|bower_components/,
-              include: path.join(__dirname, 'src'),
-              loader: 'babel-loader'
+                test: /src\/.+\.js$/,
+                exclude: /node_modules/,
+                include: PATHS.app,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015']
+                }
           }
         ]
     },
+    devtool: 'eval-source-map',
+    devServer: {
+        contentBase: './build',
+        noInfo: true, //  --no-info option
+        inline: true
+    },
     plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: true,
-            compress: {
-                warnings: false
-            }
-        }),
-        new webpack.BannerPlugin(banner)
+        // new webpack.optimize.UglifyJsPlugin({
+        //     sourceMap: true,
+        //     compress: {
+        //         warnings: false
+        //     }
+        // }),
+        // new webpack.BannerPlugin(banner),
+        // new ExtractTextPlugin('build/main.css', {
+        //     allChunks: true
+        // })
     ]
 };
+
+// var path = require("path");
+//     module.exports = {
+//         entry: {
+//         app: ['./src/main.js']
+//     },
+//     output: {
+//         path: path.resolve(__dirname, 'build'),
+//         filename: 'bundle.js'
+//     },
+//     module: {
+//         loaders: [{
+//             test: /\.jsx?$/,
+//             exclude: /(node_modules|bower_components)/,
+//             loader: 'babel', // 'babel-loader' is also a legal name to reference
+//             query: {
+//                 presets: ['react', 'es2015']
+//             }
+//         }]
+//     },
+//     plugins: [
+//         new webpack.optimize.CommonsChunkPlugin({
+//             name:      'main', // Move dependencies to our main file
+//             children:  true, // Look for common dependencies in all children,
+//             minChunks: 2, // How many times a dependency must come up before being extracted
+//         }),
+//     ],
+// };
